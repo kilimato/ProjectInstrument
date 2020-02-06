5d67bb0b341cb5f663750d445ef97a62e96ce90d
jeonalli <55280110+jeonalli@users.noreply.github.com>
2020-02-06 19:07:01 +0200

    FMOD integration and basic footstep sounds
    I integrated FMOD to the project and made a new script called testFootsteps or something along those lines. I tried to trigger footsteps based on the walking animation but it didn't work so it's based of the players characters horizontal velocity.

---
+++ InstrumentWorld/Assets/Scripts/TestFootsteps.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFootsteps : MonoBehaviour
+{
+    private CharacterController controller;
+    private FMOD.Studio.EventInstance foosteps;
+
+    float timer = 0.0f;
+
+    [SerializeField]
+    float footstepSpeed = 0.3f;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        Vector3 horizontalVelocity = controller.velocity;
+        horizontalVelocity = new Vector3(controller.velocity.x, 0, controller.velocity.z);
+
+        // The speed on the x-z plane ignoring any speed
+        float horizontalSpeed = horizontalVelocity.magnitude;
+        if (horizontalSpeed >= 0.01f)
+        {
+            if (timer > footstepSpeed)
+            {
+                PlayFootstep();
+                timer = 0.0f;
+            }
+
+            timer += Time.deltaTime;
+        }
+    }
+
+    private void PlayFootstep()
+    {
+        foosteps = FMODUnity.RuntimeManager.CreateInstance("event:/Footsteps");
+        foosteps.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        foosteps.start();
+        foosteps.release();
+    }
+}
